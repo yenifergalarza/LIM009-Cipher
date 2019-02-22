dd0912b9c20e523091494fe83c0d6d2543a6b594
yenifergalarza <yeniferalexandra2000@hotmail.com>
2019-02-22 09:39:19 -0500

    cifrado y descifrado ya completo ,css basico

--- src/cipher.js
+++ src/cipher.js
@@ -1,8 +1,25 @@
 window.cipher = {
-  encode: () => {
-    /* Acá va tu código */
+  encode: (offset,string) => {
+    offset=offset%65;
+    let asciiLetter ="";
+    for( let i =  0  ; i< string.length;i++){
+      //console.log(string[i]);
+     let LetterToAscii= string[i].charCodeAt(0);
+    asciiLetter +=  String.fromCharCode((LetterToAscii-65+offset)%26+65);
+      //palabramostrada.innerhtml 
+    }
+    return  asciiLetter  ;
   },
-  decode: () => {
-    /* Acá va tu código */
+  decode: (offset,string) => {
+    offset=offset%65;
+    let asciiLetter ="";
+    for( let i =  0  ; i< string.length;i++){
+      //console.log(string[i]);
+     let LetterToAscii= string[i].charCodeAt(0);
+    asciiLetter +=  String.fromCharCode((LetterToAscii+65-offset)%26+65);
+    //palabramostrada.innerhtml 
+    }
+    return  asciiLetter  ;
   }
 };
+
--- src/index.js
+++ src/index.js
@@ -1,1 +1,34 @@
+//import { createCipher } from "crypto";
+
 /* Acá va tu código */
+const textAreaDecipher = document.getElementById("decipher");
+const buttonDecipher = document.getElementById("decipherButton");
+const textDecipherReplace = document.getElementById("decipherText");
+const offsetNumber = document.getElementById("offset");
+//let add = () => document.getElementById("firstText").innerHTML = ` ${} `;
+const textAreaCipher = document.getElementById("cipher");
+const buttonCipher = document.getElementById("cipherButton");
+const textCipherReplace = document.getElementById("cipherText");
+
+
+
+
+buttonCipher.addEventListener("click", replaceTextjs);
+function replaceTextjs(){
+    //textCipherReplace.innerHTML  += cipher.encode(offsetNumber,textAreaDecipher);
+    console.log(`${offsetNumber}`)
+    textCipherReplace.innerHTML  += cipher.encode( offsetNumber.value,textAreaCipher.value);
+}
+
+buttonDecipher.addEventListener("click", replaceTextjsD);
+function replaceTextjsD(){
+    //textCipherReplace.innerHTML  += cipher.encode(offsetNumber,textAreaDecipher);
+    textDecipherReplace.innerHTML  += cipher.decode( offsetNumber.value,textAreaDecipher.value);
+}
+
+
+
+
+
+
+
