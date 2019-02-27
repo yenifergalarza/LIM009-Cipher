26a9cf82ff2e800fef5fdecd532174d2a0927b19
yenifergalarza <yeniferalexandra2000@hotmail.com>
2019-02-27 13:19:31 -0500

    test ya

--- test/cipher.spec.js
+++ test/cipher.spec.js
@@ -10,8 +10,23 @@ describe('cipher', () => {
       assert.equal(typeof cipher.encode, 'function');
     });
 
-    it('debería retornar "HIJKLMNOPQRSTUVWXYZABCDEFG" para "ABCDEFGHIJKLMNOPQRSTUVWXYZ" con offset 33');
-    assert.equal( cipher.encode("ABCDEFGHIJKLMNOPQRSTUVWXYZ",33), "HIJKLMNOPQRSTUVWXYZABCDEFG") ;
+    it('debería retornar "hijklmnopqrstuvwxyzabcdefg" para "ABCDEFGHIJKLMNOPQRSTUVWXYZ" con offset 33', () => {
+     assert.equal(cipher.encode(33,"ABCDEFGHIJKLMNOPQRSTUVWXYZ"),"hijklmnopqrstuvwxyzabcdefg") ;  
+    });
+
+    it('debería retornar "*+,-./0123456789:;<=>?@abc" para "ABCDEFGHIJKLMNOPQRSTUVWXYZ" con offset -23', () => {
+      assert.equal(cipher.encode(-23,"ABCDEFGHIJKLMNOPQRSTUVWXYZ"),"*+,-./0123456789:;<=>?@abc") ;  
+     });
+   
+
+     it('debería retornar "uvwxyzabcdefghijklmnopqrst" para "abcdefghijklmnopqrstuvwxyz" con offset 38012345', () => {
+      assert.equal(cipher.encode(38012345,"abcdefghijklmnopqrstuvwxyz"),"uvwxyzabcdefghijklmnopqrst");  
+     });
+   
+     it('debería retornar "abcdefghijklmnopqrstuvwxyz" para "abcdefghijklmnopqrstuvwxyz" con offset 0', () => {
+      assert.equal(cipher.encode(0,"abcdefghijklmnopqrstuvwxyz"),"abcdefghijklmnopqrstuvwxyz");  
+     });
+
     }
   );
 
@@ -21,7 +36,30 @@ describe('cipher', () => {
       assert.equal(typeof cipher.decode, 'function');
     });
 
-    it('debería retornar "ABCDEFGHIJKLMNOPQRSTUVWXYZ" para "HIJKLMNOPQRSTUVWXYZABCDEFG" con offset 33');
-  });
-
-});
+   
+    it('debería retornar "tuvwxyzabcdefghijklmnopqrs" para "ABCDEFGHIJKLMNOPQRSTUVWXYZ" con offset 33', () => {
+      assert.equal(cipher.decode(33,"ABCDEFGHIJKLMNOPQRSTUVWXYZ"),"tuvwxyzabcdefghijklmnopqrs") ;  
+     });
+ 
+     it('debería retornar "xyzabcdefghijklmnopqrstuvw" para "ABCDEFGHIJKLMNOPQRSTUVWXYZ" con offset -23', () => {
+      assert.equal(cipher.decode(-23,"ABCDEFGHIJKLMNOPQRSTUVWXYZ"),"xyzabcdefghijklmnopqrstuvw") ;  
+      });
+    
+ 
+      it('debería retornar "ghijklmnopqrstuvwxyzabcdef" para "abcdefghijklmnopqrstuvwxyz" con offset 38012345', () => {
+        assert.equal(cipher.decode(38012345,"abcdefghijklmnopqrstuvwxyz"),"ghijklmnopqrstuvwxyzabcdef");  
+       });
+     
+       it('debería retornar "abcdefghijklmnopqrstuvwxyz" para "abcdefghijklmnopqrstuvwxyz" con offset 0', () => {
+        assert.equal(cipher.decode(0,"abcdefghijklmnopqrstuvwxyz"),"abcdefghijklmnopqrstuvwxyz");  
+       });
+     
+    
+    
+    });
+    
+    
+    
+    
+    });
+    
